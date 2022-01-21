7be15c035f9d07b6e64dbe4efbcfcbf3d4a0e8f1
pepsinotjelly <1637597600@qq.com>
2022-01-22 00:47:30 +0800

    无聊

---
+++ src/main/solutions/_086_partition.java
@@ -0,0 +1,42 @@
+package solutions;
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    class _086_partition {
+        public ListNode partition(ListNode head, int x) {
+            if (head == null || head.next == null) return head;
+            ListNode cur = head;
+            ListNode small_head = new ListNode(-1);
+            ListNode large_head = new ListNode(-1);
+            ListNode small_tail = small_head;
+            ListNode large_tail = large_head;
+            while (cur != null) {
+                if (cur.val < x) {
+                    small_tail.next = cur;
+                    small_tail = small_tail.next;
+                } else {
+                    large_tail.next = cur;
+                    large_tail = large_tail.next;
+                }
+                cur = cur.next;
+            }
+            large_tail.next = null;
+            small_tail.next = large_head.next;
+            return small_head.next;
+        }
+    }
--- src/main/test/Test.java
+++ src/main/test/Test.java
@@ -1,6 +1,8 @@
+package test;
+
 public class Test {
     public static void main(String[] args) {
-        _038_count_and_say solution = new _038_count_and_say();
-        System.out.println(solution.countAndSay(5));
+//        _038_count_and_say solution = new _038_count_and_say();
+//        System.out.println(solution.countAndSay(5));
     }
 }
