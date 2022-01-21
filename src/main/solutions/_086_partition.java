e388fbf23ff2c05eb0dd064a7fc44ca5ec35b6aa
pepsinotjelly <1637597600@qq.com>
2022-01-22 00:50:46 +0800

    无聊

--- src/main/solutions/_086_partition.java
+++ src/main/solutions/_086_partition.java
@@ -1,7 +1,7 @@
 package solutions;
 
 
-public class ListNode {
+class ListNode {
     int val;
     ListNode next;
 
@@ -16,27 +16,28 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+}
 
-    class _086_partition {
-        public ListNode partition(ListNode head, int x) {
-            if (head == null || head.next == null) return head;
-            ListNode cur = head;
-            ListNode small_head = new ListNode(-1);
-            ListNode large_head = new ListNode(-1);
-            ListNode small_tail = small_head;
-            ListNode large_tail = large_head;
-            while (cur != null) {
-                if (cur.val < x) {
-                    small_tail.next = cur;
-                    small_tail = small_tail.next;
-                } else {
-                    large_tail.next = cur;
-                    large_tail = large_tail.next;
-                }
-                cur = cur.next;
+class _086_partition {
+    public ListNode partition(ListNode head, int x) {
+        if (head == null || head.next == null) return head;
+        ListNode cur = head;
+        ListNode small_head = new ListNode(-1);
+        ListNode large_head = new ListNode(-1);
+        ListNode small_tail = small_head;
+        ListNode large_tail = large_head;
+        while (cur != null) {
+            if (cur.val < x) {
+                small_tail.next = cur;
+                small_tail = small_tail.next;
+            } else {
+                large_tail.next = cur;
+                large_tail = large_tail.next;
             }
-            large_tail.next = null;
-            small_tail.next = large_head.next;
-            return small_head.next;
+            cur = cur.next;
         }
+        large_tail.next = null;
+        small_tail.next = large_head.next;
+        return small_head.next;
     }
+}
