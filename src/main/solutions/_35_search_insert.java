1d131b1bf0e655100122fa3a579137f3be73a2fe
pepsinotjelly <1637597600@qq.com>
2022-01-22 12:17:22 +0800

    无聊

---
+++ src/main/solutions/_35_search_insert.java
@@ -0,0 +1,21 @@
+package solutions;
+
+
+class _35_search_insert {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        int res = n;
+        while (low <= high) {
+            int mid = ((high - low) / 2) + low;
+            if (target <= nums[mid]) {
+                res = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return res;
+    }
+}
